3bec7deecf25e9f6f877648ab22fb5b4341c72cc
Sagnik Bose <107016185+cyberytti@users.noreply.github.com>
2025-07-22 13:11:40 +0530

    Update main_agent.py

--- backend/main_agent.py
+++ backend/main_agent.py
@@ -4,12 +4,13 @@ from typing import List, Literal
 from pydantic import BaseModel, Field
 import time
 from .functions.metadata_to_pdf import create_dpp_pdf
+#from functions.metadata_to_pdf import create_dpp_pdf
 
 questions_list=[]
 
 
 class DPPify:
-    def generate_dpp_metadata(self,topic_name: str, total_questions: int, difficulty_level: str, api_key: str, language: str = "English"):
+    def generate_dpp_metadata(self,topic_name: str, total_questions: int, question_type: str, difficulty_level: str, api_key: str, language: str = "English"):
         time.sleep(2)
         class Question(BaseModel):
             text: str
@@ -20,14 +21,27 @@ class DPPify:
             instructions: str
             questions: List[Question] = Field(f"A list of {total_questions} questions.")
 
-        with open("backend/prompts/dpp_creater_system_prompt.txt","r") as file:
+        if question_type.lower().replace(" ", "")=="onlymcq":
+            prompt_path="backend/prompts/only_mcq_creator_system_prompt.txt"
+        elif question_type.lower().replace(" ", "")=="onlysaq":
+            prompt_path="backend/prompts/only_saq_creator_system_prompt.txt"
+        elif question_type.lower().replace(" ", "")=="both":
+            prompt_path="backend/prompts/both_questions_creater_system_prompt.txt"
+        else:
+            prompt_path="backend/prompts/both_questions_creater_system_prompt.txt"
+
+        
+
+        with open(prompt_path,"r") as file:
             system_prompt=file.read()
+
 
         user_prompt=f"""
 Topic name: {topic_name}
 Language: {language} 
 Total number of questions: {total_questions} 
 Difficulty level: {difficulty_level}"""
+        
         
         agent = Agent(
             model=Cerebras(id="qwen-3-235b-a22b",api_key=api_key),
@@ -51,9 +65,10 @@ Difficulty level: {difficulty_level}"""
         return output
 
 
-    def run(self,topic_name: str, total_q: int, level: str, api_key: str):
+    def run(self,topic_name: str, question_type:str, total_q: int, level: str, api_key: str):
         dpp = self.generate_dpp_metadata(
             topic_name=topic_name,
+            question_type=question_type,
             total_questions=total_q,
             difficulty_level=level,
             api_key=api_key
@@ -71,3 +86,4 @@ Difficulty level: {difficulty_level}"""
 
 
 
+# pdf_path = DPPify().run(topic_name="megnatism numarical",question_type="saq" ,total_q=43, level="hard", api_key="csk-w44h9cpw8jcy55dv4kttxyjhy3t42rppj595epyexn245ptj")
